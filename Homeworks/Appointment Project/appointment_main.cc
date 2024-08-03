fea872a4230e712983d0acaf7b3135f41fbf9dc8
Parker-Riggs-17 <146764931+Parker-Riggs-17@users.noreply.github.com>
2024-08-03 12:30:19 -0400

    Add files via upload

---
+++ Homeworks/Appointment Project/appointment_main.cc
@@ -0,0 +1,20 @@
+/**
+ *   @file: appointment_main.cc
+ * @author: Parker Riggs
+ *   @date: 04 / 12 / 2023
+ *  @brief: This program handles the information for 1 appointment.
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include "appointment.h"
+
+using namespace std;
+
+//function prototypes
+
+int main(int argc, char const *argv[]) {
+
+    return 0;
+}// main
